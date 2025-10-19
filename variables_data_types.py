d4489470bd9cf3bf51e8e4ae11140e5442e73c22
Abhishek88788 <abhisheknagar342@gmail.com>
2025-10-20 02:50:32 +0530

    feat: adding variables & data type .py

---
+++ variables_data_types.py
@@ -0,0 +1,26 @@
+name = "abhishek" ##tpye: str
+age = 22  ##type: int
+height = 5.9  ##type: float  
+is_student = True ##type: bool
+a = None  ##type: NoneType
+
+# Printing the variables
+print("Name:", name , type(name))
+print("Age:", age , type(age))
+print("Height:", height , type(height))
+print("Is Student:", is_student , type(is_student))
+print("a:", a , type(a))
+
+##Data Types in python
+# 1. String
+# 2. Integer
+# 3. Float
+# 4. Boolean
+
+
+number = [1, 2, 3, 4, 5]  ##type: list
+print((number))
+
+fruits = ["apple", "banana", "cherry"]  ##type: list
+print(fruits[-1]) # Accessing first element
+
